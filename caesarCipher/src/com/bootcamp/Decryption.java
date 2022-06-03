7d3e8166fc0dd171c40cb8ba5183c0225b67f225
Milan Corluka <milancorluka023@gmail.com>
2022-06-03 04:49:47 +0200

    Bonus Exercise

--- caesarCipher/src/com/bootcamp/Decryption.java
+++ caesarCipher/src/com/bootcamp/Decryption.java
@@ -1,6 +1,4 @@
 package com.bootcamp;
-
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Decryption {
--- caesarCipher/src/com/bootcamp/Encryption.java
+++ caesarCipher/src/com/bootcamp/Encryption.java
@@ -1,6 +1,4 @@
 package com.bootcamp;
-
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Encryption {
